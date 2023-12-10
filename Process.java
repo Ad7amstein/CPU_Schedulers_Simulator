615fb677809f2cfa4cb5434e2c8b8974f5000fb6
Ad7amstein <adham32003200@gmail.com>
2023-12-10 16:44:06 +0200

    Update Process class

--- Process.java
+++ Process.java
@@ -1,35 +1,35 @@
 import java.util.Random;
 
-public class Process
-{
+public class Process {
     public String Name;
     public String Color;
     public int ArrivalTime;
     public int BurstTime;
     public int PriorityNumber;
     public int CompleteTime;
-    public int AG_factor ;
+    public int AG_factor;
     public int randomNumber;
     public int QuantumTime;
-    Process(String Name , String Color , int ArrivalTime , int BurstTime , int PriorityNumber , int QuantumTime)
-    {
+
+    Process(String Name, String Color, int ArrivalTime, int BurstTime, int PriorityNumber, int QuantumTime) {
         CompleteTime = 0;
         this.Name = Name;
         this.Color = Color;
         this.ArrivalTime = ArrivalTime;
-        this.BurstTime = BurstTime ;
-        this.PriorityNumber = PriorityNumber ;
-        this.QuantumTime=QuantumTime;
+        this.BurstTime = BurstTime;
+        this.PriorityNumber = PriorityNumber;
+        this.QuantumTime = QuantumTime;
         Random random = new Random();
         randomNumber = random.nextInt(21);
-        if(randomNumber < 10)
+        if (randomNumber < 10)
             AG_factor = randomNumber + ArrivalTime + AG_factor;
         else if (randomNumber > 10)
             AG_factor = 10 + ArrivalTime + AG_factor;
         else
             AG_factor = PriorityNumber + ArrivalTime + AG_factor;
     }
-    public void print(){
+
+    public void print() {
         int turnAroundTime = CompleteTime - ArrivalTime,
                 WaitingTime = turnAroundTime - BurstTime;
         System.out.printf("%-12s %-13d %d%n", Name, WaitingTime, turnAroundTime);
