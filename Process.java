5c6c9f7d8b2fed75113b4f7ff35f6c4bf3c74764
Abdullah <117462142+AbdullahFarg@users.noreply.github.com>
2023-12-11 17:28:06 +0200

    Add files via upload

--- Process.java
+++ Process.java
@@ -1,35 +1,58 @@
 import java.util.Random;
 
-public class Process {
+public class Process
+{
     public String Name;
     public String Color;
     public int ArrivalTime;
     public int BurstTime;
     public int PriorityNumber;
     public int CompleteTime;
-    public int AG_factor;
+    public int AG_factor ;
     public int randomNumber;
     public int QuantumTime;
-
-    Process(String Name, String Color, int ArrivalTime, int BurstTime, int PriorityNumber, int QuantumTime) {
-        CompleteTime = 0;
+//    @Override
+//    public String toString() {
+//        return "Process{" +
+//                "AgFactor="+AG_factor +
+//                '}';
+//    }
+    Process(String Name , String Color , int ArrivalTime , int BurstTime , int PriorityNumber , int QuantumTime )
+    {
         this.Name = Name;
         this.Color = Color;
         this.ArrivalTime = ArrivalTime;
-        this.BurstTime = BurstTime;
-        this.PriorityNumber = PriorityNumber;
-        this.QuantumTime = QuantumTime;
-        Random random = new Random();
-        randomNumber = random.nextInt(21);
-        if (randomNumber < 10)
-            AG_factor = randomNumber + ArrivalTime + AG_factor;
-        else if (randomNumber > 10)
-            AG_factor = 10 + ArrivalTime + AG_factor;
-        else
-            AG_factor = PriorityNumber + ArrivalTime + AG_factor;
-    }
+        this.BurstTime = BurstTime ;
+        this.PriorityNumber = PriorityNumber ;
+        this.QuantumTime=QuantumTime;
 
-    public void print() {
+    }
+    void CalcAgFactorAndCompleteTime  (int n , int m)
+    {
+        if(n == -1)
+        {
+            Random random = new Random();
+            randomNumber = random.nextInt(21);
+            if(randomNumber < 10)
+                AG_factor = randomNumber + ArrivalTime + AG_factor;
+            else if (randomNumber > 10)
+                AG_factor = 10 + ArrivalTime + AG_factor;
+            else
+                AG_factor = PriorityNumber + ArrivalTime + AG_factor;
+        }
+        else
+            AG_factor = n ;
+        if(m == -1)
+            CompleteTime = 0;
+        else
+            CompleteTime = m;
+    }
+    int GetAgFactor ()
+    {
+        return AG_factor;
+    }
+    public void print(){
+        System.out.println("CompleteTime = " + CompleteTime + " " + "ArrivalTime = " + ArrivalTime +" " +  "BurstTime ="+BurstTime );
         int turnAroundTime = CompleteTime - ArrivalTime,
                 WaitingTime = turnAroundTime - BurstTime;
         System.out.printf("%-12s %-13d %d%n", Name, WaitingTime, turnAroundTime);
