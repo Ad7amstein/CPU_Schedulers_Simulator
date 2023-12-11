aedfb251794021c7680f892969050307e8a3fd7d
habibaihab <101571848+habibaihab@users.noreply.github.com>
2023-12-11 21:20:45 +0200

    addPriorityScheduling

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
+import java.util.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args)
     {
         AG_Scheduler S4 = new AG_Scheduler();
+        Priority_Scheduler S3=new Priority_Scheduler();
         int NumberOfProcesses  , QuantumTime;
         Deque<Process> processes = new ArrayDeque<>();
+        List<Process> p1 = new ArrayList<>();
+
         System.out.println("Enter Number of processes : ");
         Scanner scanner = new Scanner(System.in);
         NumberOfProcesses =  scanner.nextInt();
@@ -19,17 +22,24 @@ public class Main
         for(int i = 0 ;i < NumberOfProcesses ; i++)
         {
             scanner.nextLine() ;
-            String Name = scanner.nextLine() ;
-//            String Color = scanner.nextLine() ;
-            int BurstTime = scanner.nextInt();
+            System.out.print("Name: ");
+            String Name = scanner.next();
+            System.out.print("Arrival Time: ");
             int ArrivalTime = scanner.nextInt();
+            System.out.print("Burst Time: ");
+            int BurstTime = scanner.nextInt();
+            System.out.print("Priority Number: ");
             int PriorityNumber = scanner.nextInt();
             Process p = new Process(Name , "Color" , ArrivalTime, BurstTime , PriorityNumber , QuantumTime);
             p.CalcAgFactorAndCompleteTime(-1,-1);
             processes.addLast(p);
+            Process p2= new Process(Name , ArrivalTime, BurstTime , PriorityNumber);
+            p1.add(p2);
         }
+        S3.priorityScheduling(p1);
         S4.fun(processes);
         Iterator<Process> iterator = S4.complete_list.iterator();
+
         while (iterator.hasNext())
         {
             iterator.next().print();
